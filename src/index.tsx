57800eda5eb25902d617bcc5239a49bc7e475745
aoi takuto <a.013053@gmail.com>
2020-05-01 23:30:26 +0900

    split theme file from index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import CssBaseline from '@material-ui/core/CssBaseline'
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
-import { grey } from '@material-ui/core/colors'
+import { ThemeProvider } from '@material-ui/styles'
+import { theme } from './theme'
 
-const theme = createMuiTheme({
-    palette: {
-        background: {
-            default: grey[300],
-        },
-    },
-    overrides: {},
-})
 ReactDOM.render(
     <React.StrictMode>
         <ThemeProvider theme={theme}>
--- src/theme.ts
+++ src/theme.ts
@@ -1,1 +1,11 @@
-export const theme = {}
+import { createMuiTheme } from '@material-ui/core'
+import { grey } from '@material-ui/core/colors'
+
+export const theme = createMuiTheme({
+    palette: {
+        background: {
+            default: grey[300],
+        },
+    },
+    overrides: {},
+})
